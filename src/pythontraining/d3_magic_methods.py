0a6ccb45568cbae1719bf57bef0d189bb520d31c
Joris Hoendervangers <joris@pythonsherpa.com>
2022-06-02 11:18:08 +0200

    demo magic methods

---
+++ src/pythontraining/d3_magic_methods.py
@@ -0,0 +1,20 @@
+"""
+Demo of magic methods
+"""
+
+
+class BankAccount:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+
+    def __str__(self):
+        return f"Bank account of {self.owner}"
+
+    def __gt__(self, other):
+        return self.balance > other.balance
+
+
+account_julia = BankAccount("Julia", 100)
+account_timo = BankAccount("Timo", 50)
+print(account_julia > account_timo)
