7bbd51e0a4e1198a39f5b7e30636b325282c11b3
Joris Hoendervangers <joris@pythonsherpa.com>
2022-06-03 14:38:10 +0200

    demo pandas

--- src/pythontraining/d15_pandas.py
+++ src/pythontraining/d15_pandas.py
@@ -2,7 +2,13 @@
 Demo of the pandas library
 """
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.DataFrame(data=[[1, 2, 3], [4, 5, 6], [7, 8, 9]], columns=["A", "B", "C"])
-
+# df = pd.DataFrame(data=[[1, 2, 3], [4, 5, 6], [7, 8, 9]], columns=["A", "B", "C"])
 df = pd.read_csv("https://raw.githubusercontent.com/pythonsherpa/data/master/sales.csv")
+
+df["Profits"] = df["Sales"] - df["Costs"]
+df.nlargest(10, columns=["Profits"]).plot(kind="bar", x="Country")
+plt.show()
+
+df.to_csv("sales.csv")
